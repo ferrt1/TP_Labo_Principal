043d6f56db81b0d343a86713198fe12f627d1ec8
Fernando <fernandotrejo125@gmail.com>
2024-04-21 18:09:47 -0300

    Agregando pantallas

--- Cypher_Vault/desarrollo/app/app/src/main/java/com/example/cypher_vault/MainActivity.kt
+++ Cypher_Vault/desarrollo/app/app/src/main/java/com/example/cypher_vault/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.example.cypher_vault
 
+import MyApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.lifecycle.ProcessCameraProvider
-import com.example.cypher_vault.model.CameraModel
-import com.example.cypher_vault.view.LoginView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-            LoginView(cameraProviderFuture, this)
+            MyApp()
         }
     }
 }
--- Cypher_Vault/desarrollo/app/app/src/main/java/com/example/cypher_vault/view/LoginView.kt
+++ Cypher_Vault/desarrollo/app/app/src/main/java/com/example/cypher_vault/view/LoginView.kt
@@ -1,64 +1,51 @@
-package com.example.cypher_vault.view
-
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
-import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.common.util.concurrent.ListenableFuture
 
-
 @Composable
-fun CameraButton(isCameraOpen: MutableState<Boolean>) {
-    val context = LocalContext.current
-    Button(onClick = {
-        // Verifica si se tienen los permisos necesarios
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            // Inicia la cámara
-            isCameraOpen.value = true
-        } else {
-            Toast.makeText(context, "Permiso de cámara no concedido", Toast.LENGTH_LONG).show()
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "login") {
+        composable("login") {
+            LoginView(navController)
         }
-    }) {
-        Text("Registrarse")
+        composable("camera") {
+            CameraScreen(navController)
+        }
+        composable("confirmation") {
+            ConfirmationScreen()
+        }
     }
 }
 
-
 @Composable
-fun LoginView(cameraProviderFuture: ListenableFuture<ProcessCameraProvider>, lifecycleOwner: LifecycleOwner) {
-    val isCameraOpen = remember { mutableStateOf(false) }
-    val cameraSelector = CameraSelector.Builder()
-        .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
-        .build()
-
-    val cameraProvider = cameraProviderFuture.get()
-    val preview = Preview.Builder().build()
-    cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
-
+fun LoginView(navController: NavController) {
     // Agrega estados para los campos de texto
     val emailState = remember { mutableStateOf(TextFieldValue()) }
     val nameState = remember { mutableStateOf(TextFieldValue()) }
@@ -83,16 +70,63 @@ fun LoginView(cameraProviderFuture: ListenableFuture<ProcessCameraProvider>, lif
             )
 
             // Botón de la cámara
-            CameraButton(isCameraOpen)
-
-            // Vista previa de la cámara
-            if (isCameraOpen.value) {
-                CameraPreview(preview)
+            Button(onClick = { navController.navigate("camera") }) {
+                Text("Registrarse")
             }
         }
     }
 }
 
+@Composable
+fun CameraScreen(navController: NavController) {
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val isCameraOpen = remember { mutableStateOf(true) }
+    val cameraSelector = CameraSelector.Builder()
+        .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+        .build()
+
+    val cameraProvider = cameraProviderFuture.get()
+    val preview = Preview.Builder().build()
+
+    LaunchedEffect(cameraProviderFuture) {
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        if (isCameraOpen.value) {
+            CameraPreview(preview)
+            CloseCameraButton(isCameraOpen, cameraProvider, navController)
+        }
+    }
+}
+
+
+@Composable
+fun CloseCameraButton(isCameraOpen: MutableState<Boolean>, cameraProvider: ProcessCameraProvider, navController: NavController) {
+    Button(onClick = {
+        // Cierra la cámara
+        isCameraOpen.value = false
+        cameraProvider.unbindAll()
+        navController.navigate("confirmation")
+    }) {
+        Text("Tomar foto")
+    }
+}
+
+@Composable
+fun ConfirmationScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("¡Ya estás registrado! Iniciar sesión")
+    }
+}
 
 @Composable
 fun CameraPreview(preview: Preview) {
