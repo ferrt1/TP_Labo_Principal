9ba67835d134529bfea6c90f9f889f33814c5e7a
Fernando <fernandotrejo125@gmail.com>
2024-04-27 17:47:06 -0300

    Empezando pantalla logeo

--- Cypher_Vault/desarrollo/app/app/src/main/java/com/example/cypher_vault/view/login/LoginList.kt
+++ Cypher_Vault/desarrollo/app/app/src/main/java/com/example/cypher_vault/view/login/LoginList.kt
@@ -4,6 +4,9 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -21,13 +24,89 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.sp
+import com.example.cypher_vault.R
 import com.example.cypher_vault.controller.authentication.AuthenticationController
 import com.example.cypher_vault.view.registration.CameraPreview
+
+
+val firstColor = Color(0xFF02a6c3)
+val secondColor = Color(0xFF01243a)
+val thirdColor = Color(0xFF005767)
+
+@Composable
+fun CustomTitle() {
+    val textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 54.sp)
+
+    Row(modifier = Modifier.wrapContentSize().offset(16.dp).padding( top = 40.dp), )
+        {
+        Column() {
+            Row (
+                horizontalArrangement = Arrangement.Absolute.Left
+            ){
+                Text(
+                    text = "C",
+                    color = firstColor,
+                    style = textStyle,
+                )
+                Text(
+                    text = "ypher",
+                    color = secondColor,
+                    style = textStyle,
+                )
+            }
+            Row {
+                Text(
+                    text = "V",
+                    color = firstColor,
+                    style = textStyle,
+                )
+                Text(
+                    text = "ault",
+                    color = secondColor,
+                    style = textStyle,
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(-(32).dp.roundToPx(), 14.dp.roundToPx()) }
+                .size(94.dp)
+                .align(Alignment.Bottom)
+                .border(2.dp, firstColor, RoundedCornerShape(50))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .size(82.dp)
+                    .align(Alignment.Center)
+            )
+        }
+    }
+
+}
+
 
 @Composable
 fun NavigationLogin(authenticationController: AuthenticationController) {
@@ -35,21 +114,38 @@ fun NavigationLogin(authenticationController: AuthenticationController) {
 
     var selectedPersona by remember { mutableStateOf<String?>(null) }
 
+    val textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 25.sp, color = thirdColor)
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    )
+    {
+        CustomTitle()
+        Text(
+            "Inicio de sesión",
+            style = textStyle,
+            modifier = Modifier.padding( start = 65.dp, top = 70.dp ).align(Alignment.Start),
+        )
+    }
+
+
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+
         items(users) { user ->
             Button(
                 onClick = {
                     selectedPersona = user.firstName
                 },
-                modifier = Modifier.padding(8.dp) // Añade un espacio entre los botones
+                modifier = Modifier.padding(8.dp)
             ) {
-                Text(text = user.firstName ?: "")
+                Text(text = user.firstName ?: "", style = MaterialTheme.typography.bodyMedium)
             }
         }
-
     }
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -60,23 +156,20 @@ fun NavigationLogin(authenticationController: AuthenticationController) {
             border = BorderStroke(0.dp, Color.Transparent),
             modifier = Modifier.padding(8.dp)
         ) {
-            Text("Registrarse")
+            Text("Registrarse", style = MaterialTheme.typography.bodySmall)
         }
     }
 
-
-
     selectedPersona?.let { user ->
-        loginCamera(
+        LoginCamera(
             authenticationController = authenticationController,
             user = user
         )
     }
-
 }
 
 @Composable
-fun loginCamera(authenticationController: AuthenticationController, user: String) {
+fun LoginCamera(authenticationController: AuthenticationController, user: String) {
     val context = LocalContext.current
     val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
     val lifecycleOwner = LocalLifecycleOwner.current
