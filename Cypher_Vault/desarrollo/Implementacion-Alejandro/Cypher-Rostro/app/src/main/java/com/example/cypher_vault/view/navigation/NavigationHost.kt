0064c4b5a5f29d7794e496bd2ba9d00dfb5e9e06
Alejandro Moras <spectrelonewolf@gmail.com>
2024-05-01 13:25:20 -0300

    Se explota de logs el NAvigationHost

--- Cypher_Vault/desarrollo/Implementacion-Alejandro/Cypher-Rostro/app/src/main/java/com/example/cypher_vault/view/navigation/NavigationHost.kt
+++ Cypher_Vault/desarrollo/Implementacion-Alejandro/Cypher-Rostro/app/src/main/java/com/example/cypher_vault/view/navigation/NavigationHost.kt
@@ -1,6 +1,7 @@
 package com.example.cypher_vault.view.navigation
 
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,24 +19,33 @@ fun NavigationHost() {
     val authenticationController = AuthenticationController(navController)
     NavHost(navController, startDestination = "register") {
         composable("register") {
+            Log.d("faceDetection", "Inicio InitialScreen")
             InitialScreen(authenticationController)
+            Log.d("faceDetection", "Salida InitialScreen")
         }
         composable("camera/{userId}") { backStackEntry ->
             val userId = backStackEntry.arguments?.getString("userId")?.toLongOrNull()
             if (userId != null) {
+                Log.d("faceDetection", "Inicio CameraPreviewScreen")
                 CameraPreviewScreen(authenticationController, userId)
+                Log.d("faceDetection", "Salida CameraPreviewScreen")
             } else {
                 // Manejar el caso en que el userId no se pudo recuperar
             }
         }
         composable("confirmation") {
+            Log.d("faceDetection", "Inicio ConfirmationScreen")
             ConfirmationScreen(authenticationController)
+            Log.d("faceDetection", "Salida ConfirmationScreen")
         }
         composable("login") {
+            Log.d("faceDetection", "Inicio login")
             // Aquí puedes agregar la vista de inicio de sesión
         }
         composable("list") {
+            Log.d("faceDetection", "Inicio NavigationLogin")
             NavigationLogin(authenticationController)
+            Log.d("faceDetection", "Salida NavigationLogin")
         }
 
     }
