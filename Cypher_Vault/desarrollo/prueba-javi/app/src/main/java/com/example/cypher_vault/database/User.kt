d2ca24d941349c36f1ba19ac876bf1b713ee2370
javier221 <javi_b_galeano@hotmail.com>
2024-05-11 17:01:10 -0300

    tomo el contenido de Fer y realizo pruebas

---
+++ Cypher_Vault/desarrollo/prueba-javi/app/src/main/java/com/example/cypher_vault/database/User.kt
@@ -0,0 +1,14 @@
+package com.example.cypher_vault.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey val uid: String,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "email") val email: String?,
+    @ColumnInfo(name = "entry_date") val entryDate: Long, // Fecha de ingreso
+    @ColumnInfo(name = "pin") val pin: Int? // PIN del usuario
+)
---
+++ Cypher_Vault/desarrollo/prueba-javi/app/src/main/java/com/example/cypher_vault/view/navigation/NavigationHost.kt
@@ -0,0 +1,75 @@
+package com.example.cypher_vault.view.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cypher_vault.controller.authentication.AuthenticationController
+import com.example.cypher_vault.view.login.ConfirmationLoginScreen
+import com.example.cypher_vault.view.login.LoginCamera
+import com.example.cypher_vault.view.login.NavigationLogin
+import com.example.cypher_vault.view.registration.ConfirmationScreen
+import com.example.cypher_vault.view.registration.Gallery
+import com.example.cypher_vault.view.registration.InitialScreen
+import com.example.cypher_vault.view.registration.RegistrationCameraScreen
+
+@Composable
+fun NavigationHost() {
+    val navController = rememberNavController()
+    val authenticationController = AuthenticationController(navController)
+    NavHost(navController, startDestination = "register") {
+        composable("register") {
+            InitialScreen(authenticationController)
+        }
+        composable("camera/{userId}") { backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId")
+            if (userId != null) {
+                RegistrationCameraScreen(authenticationController, userId)
+            } else {
+                // Manejar el caso en que el userId no se pudo recuperar
+            }
+        }
+        composable("cameralogin/{userId}") { backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId")
+            if (userId != null) {
+                LoginCamera(authenticationController, userId)
+            } else {
+                // Manejar el caso en que el userId no se pudo recuperar
+            }
+        }
+
+        composable("confirmation/{userId}") {backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId")
+            if (userId != null) {
+                ConfirmationScreen(authenticationController, userId)
+            } else {
+                // Manejar el caso en que el userId no se pudo recuperar
+            }
+        }
+        composable("login") {
+            // Aquí puedes agregar la vista de inicio de sesión
+        }
+
+        composable("authenticate/{userId}") {backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId")
+            if (userId != null) {
+                ConfirmationLoginScreen(authenticationController, userId)
+            } else {
+                // Manejar el caso en que el userId no se pudo recuperar
+            }
+        }
+        composable("gallery/{userId}") {backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId")
+            if (userId != null) {
+                Gallery(authenticationController, userId)
+            } else {
+                // Manejar el caso en que el userId no se pudo recuperar
+            }
+        }
+        composable("list") {
+            NavigationLogin(authenticationController)
+        }
+
+    }
+}
