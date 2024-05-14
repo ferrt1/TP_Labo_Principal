6f4c1616e734d8c3fc4ba01739fb6c577c2cd6e7
Alejandro Moras <spectrelonewolf@gmail.com>
2024-05-14 19:59:41 -0300

    Borro todo me hinche las ...

--- Cypher_Vault/desarrollo/app - galeri/app/src/main/java/com/example/cypher_vault/view/viewmodel/SharedViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cypher_vault.view.viewmodel
-
-import android.graphics.Bitmap
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class SharedViewModel : ViewModel() {
-    val capturedImages = MutableLiveData<List<Bitmap>>()
-}
